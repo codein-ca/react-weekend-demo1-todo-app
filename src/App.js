4f0d36b01ff6c65c940ae0d1f698ddc36bb353e8
Daniel Santos <daniel@w-b.dev>
2019-09-03 11:01:11 -0400

    Uses ref. Clear input after saving. Format issues.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import React from "react";
-import "./App.css";
-import Task from "./Task";
+import React from 'react'
+import './App.css'
+import Task from './Task'
 
 const Tasks = props => {
-  console.log("TASKS props updated", props);
+  console.log('TASKS props updated', props)
   // TODO: note the inconsistency between states
   // The parent/container was not informed of child's actions
   // So 'completion' updates the child's UI but not the parent state.
-  return props.in.map((e, i) => <Task in={e} key={i} />);
-};
+  return props.in.map((e, i) => <Task in={e} key={i}/>)
+}
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      currentTaskTitle: "",
+      currentTaskTitle: '',
       taskList: [
         {
-          title: "Buy more diapers",
-          complete: false
+          title: 'Buy more diapers',
+          complete: false,
         },
         {
-          title: "Commit small and often",
-          complete: true
+          title: 'Commit small and often',
+          complete: true,
         },
         {
-          title: "Feed Barbara",
-          complete: false
-        }
-      ]
-    };
+          title: 'Feed Barbara',
+          complete: false,
+        },
+      ],
+    }
 
-    this.updateCurrentTask = this.updateCurrentTask.bind(this);
-    this.addTask = this.addTask.bind(this);
+    // this.updateCurrentTask = this.updateCurrentTask.bind(this)
+    this.addTask = this.addTask.bind(this)
   }
 
-  updateCurrentTask(event) {
-    this.setState({ currentTaskTitle: event.target.value });
-  }
-
-  addTask() {
-    // console.info("A task will only be added if not empty");
-    if (this.state.currentTaskTitle !== "") {
-      const local = this.state.taskList;
+  addTask () {
+    if (this.state.currentTaskTitle && this.state.currentTaskTitle === this.refs.taskTitle.value) {
+      const local = this.state.taskList
       local.push({
         title: this.state.currentTaskTitle,
-        complete: false
-      });
-      this.setState({ taskList: local });
-      this.setState({ currentTaskTitle: "" });
+        complete: false,
+      })
+      this.setState({ taskList: local })
+      this.setState({ currentTaskTitle: '' })
+      this.refs.taskTitle.value = ''
+    } else {
+      this.setState({ currentTaskTitle: this.refs.taskTitle.value })
     }
   }
 
-  render() {
+  render () {
     return (
       <div className="App">
         <header>
@@ -61,16 +59,17 @@ class App extends React.Component {
             type="text"
             placeholder="What is the task?"
             value={this.currentTaskTitle}
-            onChange={this.updateCurrentTask}
+            onBlur={this.addTask}
+            ref="taskTitle"
           />
-          <input type="button" value="Save" onClick={this.addTask} />
+          <input type="button" value="Save" onClick={this.addTask}/>
         </header>
         <ul>
-          <Tasks in={this.state.taskList} />
+          <Tasks in={this.state.taskList}/>
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
